60d56f17bc05ba0b902e51e370004aceb611f7ee
Rafael-cod <69343624+Rafael-cod@users.noreply.github.com>
2020-10-10 20:12:29 +0530

    Add files via upload

---
+++ Division.js
@@ -0,0 +1,24 @@
+class Division {
+    constructor(x, y, width, height) {
+      var options = {
+          isStatic:true
+      }
+      this.body = Matter.Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.x = x;
+      this.y = y;
+      World.add(world,this.body);
+
+    }
+    display(){
+      var pos = this.body.position;
+      push();
+      rectMode(CENTER);
+      translate(pos.x,pos.y);
+      fill("brown");
+      rect(0,0,this.width,this.height);
+      pop();
+    }
+  }
+  
